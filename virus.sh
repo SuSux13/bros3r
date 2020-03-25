4cd619cf16757def3f2785c1e4939e55f7a69cf1
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-03-25 19:21:17 +0100

    Update virus.sh

--- virus.sh
+++ virus.sh
@@ -1,30 +1,30 @@
-proxychains4 nmap -Pn -sV -v -iR 10000 --scanflags TCPACKSYNRSTFIN -p 80 -D s6cco2jylmxqcdeh.onion society44nlbxqdz.onion lulzwrzcle5ks3se.onion
+proxychains4 nmap -Pn -sV -v -iR 10000 --scanflags TCPACKSYNRSTFIN -p 80,465,993,995,25,143,110,9050,9150 -D s6cco2jylmxqcdeh.onion society44nlbxqdz.onion lulzwrzcle5ks3se.onion
 
-proxychains4 nmap -Pn  -T4 -sV -sC -v --scanflags TCPACKSYNRSTFIN -p 80 -D s6cco2jylmxqcdeh.onion  society44nlbxqdz.onion lulzwrzcle5ks3se.onion
-proxychains4 nmap -Pn -sV -v --scanflags TCPACKSYNRSTFIN -T4 -PP  -p 80 -D  society44nlbxqdz.onion society44nlbxqdz.onion lulzwrzcle5ks3se.onion
+proxychains4 nmap -Pn  -T4 -sV -sC -v --scanflags TCPACKSYNRSTFIN -p 80,465,993,995,25,143,110,9050,9150 -D s6cco2jylmxqcdeh.onion  society44nlbxqdz.onion lulzwrzcle5ks3se.onion
+proxychains4 nmap -Pn -sV -v --scanflags TCPACKSYNRSTFIN -T4 -PP  -p 80,465,993,995,25,143,110,9050,9150 -D  society44nlbxqdz.onion society44nlbxqdz.onion lulzwrzcle5ks3se.onion
 
- proxychains4 nmap -Pn -sV -v --scanflags TCPACKSYNRSTFIN -T4 -PP -p 80  society44nlbxqdz.onion--packet-trace
+ proxychains4 nmap -Pn -sV -v --scanflags TCPACKSYNRSTFIN -T4 -PP -p 80,465,993,995,25,143,110,9050,9150  society44nlbxqdz.onion--packet-trace
  
- proxychains4 nmap -Pn  -T4 -sV -sC -v --scanflags TCPACKSYNRSTFIN -p 80 s6cco2jylmxqcdeh.onion
+ proxychains4 nmap -Pn  -T4 -sV -sC -v --scanflags TCPACKSYNRSTFIN -p 80,465,993,995,25,143,110,9050,9150 s6cco2jylmxqcdeh.onion
  
 
- proxychains4 nmap -Pn  -T4 -sV -sC -v --scanflags TCPACKSYNRSTFIN -p 80 lulzwrzcle5ks3se.onion
+ proxychains4 nmap -Pn  -T4 -sV -sC -v --scanflags TCPACKSYNRSTFIN -p 80,465,993,995,25,143,110,9050,9150 lulzwrzcle5ks3se.onion
 
-proxychains4 nmap -Pn -v --scanflags TCPACKSYNRSTFIN -p 80 --packet-trace --version-trace -PP -T4 society44nlbxqdz.onion
+proxychains4 nmap -Pn -v --scanflags TCPACKSYNRSTFIN -p 80,465,993,995,25,143,110,9050,9150 --packet-trace --version-trace -PP -T4 society44nlbxqdz.onion
 
 
-proxychains4 nmap -Pn --ttl -T4 -v -sV  -p 80 -PP  --scanflags TCPACKSYNRSTFIN -iR 10000 --packet-trace society44nlbxqdz.onion
+proxychains4 nmap -Pn --ttl -T4 -v -sV  -p 80,465,993,995,25,143,110,9050,9150  --packet-trace society44nlbxqdz.onion
 
-proxychains4 nmap -Pn -T4 -p 80 --PP -sV -v --send-ip  society44nlbxqdz.onion
+proxychains4 nmap -Pn -T4 -p 80,465,993,995,25,143,110,9050,91500 --PP -sV -v --send-ip  society44nlbxqdz.onion
 
-proxychains4 nmap -Pn -sV -v -iR 10000 --scanflags TCPACKSYNRSTFIN -p 80
+proxychains4 nmap -Pn -sV -v -iR 10000 --scanflags TCPACKSYNRSTFIN -p 80 society44nlbxqdz.onion
 
 
 
 proxychains4 nmap -Pn -T4 --scanflags TCPACKSYNRSTFIN -iR-10000 -PA6,80,21,25,3306,22,110 -PP -sV -v --send-ip society44nlbxqdz.onion  --packet-trace
 
 
-proxychains4 nmap -n -Pn -T4 -F -d --version-trace -p 80 society44nlbxqdz.onion
+proxychains4 nmap -n -Pn -T4 -F -d --version-trace -p 80,465,993,995,25,143,110,9050,9150 society44nlbxqdz.onion
 
 nmap -n --ttl society44nlbxqdz.onion
 
