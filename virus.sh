76b7e072c2f1d031138452d9becc4f7acb4d4c33
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-03-26 14:07:49 +0100

    Update virus.sh

--- virus.sh
+++ virus.sh
@@ -3,28 +3,28 @@ proxychains4 nmap -Pn -sV -v  -p 80,465,993,995,25,143,110,9050,9150 -D s6cco2jy
 proxychains4 nmap -Pn  -T4 -sV -sC -v -p 80,465,993,995,25,143,110,9050,9150 -D s6cco2jylmxqcdeh.onion  society44nlbxqdz.onion lulzwrzcle5ks3se.onion
 proxychains4 nmap -Pn -sV -v-T4 -PP  -p 80,465,993,995,25,143,110,9050,9150 -D  society44nlbxqdz.onion society44nlbxqdz.onion lulzwrzcle5ks3se.onion
 
- proxychains4 nmap -Pn -sV -v  -T4 -PP -p 80,465,993,995,25,143,110,9050,9150  society44nlbxqdz.onion--packet-trace
+ proxychains4 nmap -Pn -sV -v  -T4 -PP -p 80,465,993,995,25,143,110,9050,9150 -D society44nlbxqdz.onion exwljei3bfvchv6p.onion y5afgm36mnjfdm6k7dohphuaig3xuvxhlus3uegj75twrbdped4i7hid.onion lesbianc5ldxbex4.onion  t7vxntg5oavfioj2ommhgaecglnjecek24vx7y2ap2r767de6mhyegyd.onion  5fzznwkpwdkb6esxd3d2fhc7qaz7igfhi6zlrklj6odg7fhuvu64d6ad.onion  lolitaabntti3lvs.onion dzlvexlt66pxzfmi4ajfpgrv5dawtyicxisqueg224mujlbxetkawbad.onion   wgh5kkl4kolzw7ac73eqoztv3f5hpbo4nydcp7h5muhivi2o5opknlad.onion   pedofio5ciij2nl2.onion  familybw6azkhjsc.onion                  --packet-trace
  
- proxychains4 nmap -Pn  -T4 -sV -sC -v -p 80,465,993,995,25,143,110,9050,9150 s6cco2jylmxqcdeh.onion
+ proxychains4 nmap -Pn  -T4 -sV -sC -v -p 80,465,993,995,25,143,110,9050,9150 s6cco2jylmxqcdeh.onion exwljei3bfvchv6p.onion y5afgm36mnjfdm6k7dohphuaig3xuvxhlus3uegj75twrbdped4i7hid.onion lesbianc5ldxbex4.onion t7vxntg5oavfioj2ommhgaecglnjecek24vx7y2ap2r767de6mhyegyd.onion 5fzznwkpwdkb6esxd3d2fhc7qaz7igfhi6zlrklj6odg7fhuvu64d6ad.onion  lolitaabntti3lvs.onion  dzlvexlt66pxzfmi4ajfpgrv5dawtyicxisqueg224mujlbxetkawbad.onion  wgh5kkl4kolzw7ac73eqoztv3f5hpbo4nydcp7h5muhivi2o5opknlad.onion  pedofio5ciij2nl2.onion familybw6azkhjsc.onion                                
  
 
- proxychains4 nmap -Pn  -T4 -sV -sC -v -p 80,465,993,995,25,143,110,9050,9150 lulzwrzcle5ks3se.onion
+ proxychains4 nmap -Pn  -T4 -sV -sC -v -p 80,465,993,995,25,143,110,9050,9150 -D lulzwrzcle5ks3se.onion exwljei3bfvchv6p.onion y5afgm36mnjfdm6k7dohphuaig3xuvxhlus3uegj75twrbdped4i7hid.onion lesbianc5ldxbex4.onion t7vxntg5oavfioj2ommhgaecglnjecek24vx7y2ap2r767de6mhyegyd.onion 5fzznwkpwdkb6esxd3d2fhc7qaz7igfhi6zlrklj6odg7fhuvu64d6ad.onion lolitaabntti3lvs.onion   dzlvexlt66pxzfmi4ajfpgrv5dawtyicxisqueg224mujlbxetkawbad.onion wgh5kkl4kolzw7ac73eqoztv3f5hpbo4nydcp7h5muhivi2o5opknlad.onion pedofio5ciij2nl2.onion familybw6azkhjsc.onio                             
 
-proxychains4 nmap -Pn -v -p 80,465,993,995,25,143,110,9050,9150 --packet-trace --version-trace -PP -T4 society44nlbxqdz.onion
-
-
-proxychains4 nmap -Pn --ttl -T4 -v -sV  -p 80,465,993,995,25,143,110,9050,9150  --packet-trace society44nlbxqdz.onion
-
-proxychains4 nmap -Pn -T4 -p 80,465,993,995,25,143,110,9050,91500 --PP -sV -v --send-ip  society44nlbxqdz.onion
-
-proxychains4 nmap -Pn -sV -v -iR 10000 --scanflags TCPACKSYNRSTFIN -p 80 society44nlbxqdz.onion
+proxychains4 nmap -Pn -v -p 80,465,993,995,25,143,110,9050,9150 --packet-trace --version-trace -PP -T4 -D society44nlbxqdz.onion exwljei3bfvchv6p.onion y5afgm36mnjfdm6k7dohphuaig3xuvxhlus3uegj75twrbdped4i7hid.onion lesbianc5ldxbex4.onion t7vxntg5oavfioj2ommhgaecglnjecek24vx7y2ap2r767de6mhyegyd.onion   5fzznwkpwdkb6esxd3d2fhc7qaz7igfhi6zlrklj6odg7fhuvu64d6ad.onion  lolitaabntti3lvs.onion  dzlvexlt66pxzfmi4ajfpgrv5dawtyicxisqueg224mujlbxetkawbad.onion wgh5kkl4kolzw7ac73eqoztv3f5hpbo4nydcp7h5muhivi2o5opknlad.onion  pedofio5ciij2nl2.onion  familybw6azkhjsc.onion         
 
 
+proxychains4 nmap -Pn --ttl -T4 -v -sV  -p 80,465,993,995,25,143,110,9050,9150  --packet-trace -D society44nlbxqdz.onion exwljei3bfvchv6p.onion y5afgm36mnjfdm6k7dohphuaig3xuvxhlus3uegj75twrbdped4i7hid.onion lesbianc5ldxbex4.onion   t7vxntg5oavfioj2ommhgaecglnjecek24vx7y2ap2r767de6mhyegyd.onion   5fzznwkpwdkb6esxd3d2fhc7qaz7igfhi6zlrklj6odg7fhuvu64d6ad.onion    lolitaabntti3lvs.onion  dzlvexlt66pxzfmi4ajfpgrv5dawtyicxisqueg224mujlbxetkawbad.onion wgh5kkl4kolzw7ac73eqoztv3f5hpbo4nydcp7h5muhivi2o5opknlad.onion  pedofio5ciij2nl2.onion familybw6azkhjsc.onion                
+
+proxychains4 nmap -Pn -T4 -p 80,465,993,995,25,143,110,9050,91500 --PP -sV -v --send-ip -D society44nlbxqdz.onion exwljei3bfvchv6p.onion y5afgm36mnjfdm6k7dohphuaig3xuvxhlus3uegj75twrbdped4i7hid.onion lesbianc5ldxbex4.onion  t7vxntg5oavfioj2ommhgaecglnjecek24vx7y2ap2r767de6mhyegyd.onion   5fzznwkpwdkb6esxd3d2fhc7qaz7igfhi6zlrklj6odg7fhuvu64d6ad.onion  lolitaabntti3lvs.onion dzlvexlt66pxzfmi4ajfpgrv5dawtyicxisqueg224mujlbxetkawbad.onion   wgh5kkl4kolzw7ac73eqoztv3f5hpbo4nydcp7h5muhivi2o5opknlad.onion   pedofio5ciij2nl2.onion  familybw6azkhjsc.onion     
+
+proxychains4 nmap -Pn -sV -v  --scanflags TCPACKSYNRSTFIN -p 80 -D society44nlbxqdz.onion exwljei3bfvchv6p.onion y5afgm36mnjfdm6k7dohphuaig3xuvxhlus3uegj75twrbdped4i7hid.onion lesbianc5ldxbex4.onion   t7vxntg5oavfioj2ommhgaecglnjecek24vx7y2ap2r767de6mhyegyd.onion    5fzznwkpwdkb6esxd3d2fhc7qaz7igfhi6zlrklj6odg7fhuvu64d6ad.onion  lolitaabntti3lvs.onion  dzlvexlt66pxzfmi4ajfpgrv5dawtyicxisqueg224mujlbxetkawbad.onion  wgh5kkl4kolzw7ac73eqoztv3f5hpbo4nydcp7h5muhivi2o5opknlad.onion  pedofio5ciij2nl2.onion  familybw6azkhjsc.onion                       
 
 
 
 
-proxychains4 nmap -n -Pn -T4 -F -d --version-trace -p 80,465,993,995,25,143,110,9050,9150 society44nlbxqdz.onion
+
+
+proxychains4 nmap -n -Pn -T4 -F -d --version-trace -p 80,465,993,995,25,143,110,9050,9150 society44nlbxqdz.onion exwljei3bfvchv6p.onion y5afgm36mnjfdm6k7dohphuaig3xuvxhlus3uegj75twrbdped4i7hid.onion   lesbianc5ldxbex4.onion  t7vxntg5oavfioj2ommhgaecglnjecek24vx7y2ap2r767de6mhyegyd.onion  lolitaabntti3lvs.onion dzlvexlt66pxzfmi4ajfpgrv5dawtyicxisqueg224mujlbxetkawbad.onion  wgh5kkl4kolzw7ac73eqoztv3f5hpbo4nydcp7h5muhivi2o5opknlad.onion pedofio5ciij2nl2.onion familybw6azkhjsc.onion                    
 
 nmap -n --ttl society44nlbxqdz.onion
 
